27e4c56aa10bb1dd3bcf594dd0884bb987ebc8af
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-18 13:23:56 -0800

    completed

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 // TODO
-var TodoList = (props) => {
-    var onListItemClick = (event) => {
+class TodoListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false,
+            bold: false,
+        };
+    }
+    hover() {
+
+    }
+    onListItemClick() {
+        this.setState({
+            done: !this.state.done
+        })
         console.log('I got Clicked')
     }
+
+    onMouseDown() {
+        this.setState({
+            bold: !this.state.bold
+        })
+    }
+
+    render() {
+
+        var style = {
+            textDecoration: this.state.done ? 'line-through' : 'none',
+            fontWeight: this.state.bold ? 'bold' : 'normal'
+        };
+
+        return (
+            <li style={style} onClick={this.onListItemClick.bind(this)} onMouseEnter={this.onMouseDown.bind(this)} onMouseLeave={this.onMouseDown.bind(this)}>{this.props.todo}</li>
+        );
+    }
+}
+var TodoList = (props) => {
+    // var onListItemClick = (event) => {
+    //     console.log('I got Clicked')
+    // }
     return (
     <ul>
-        <li onClick={onListItemClick}>{props.todos[0]}</li>
-        <li>{props.todos[1]}</li>
-        <li>{props.todos[2]}</li>
+        {props.todos.map(todo => <TodoListItem todo={todo} />)}
     </ul>
     )
 };
 var App = () => (
     <div>
         <h1>List</h1>
-        <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+        <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']} />
         
     </div>    
     );

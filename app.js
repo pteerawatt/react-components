8955f2f07e237ce1d5c4982ea68765d6a0136d38
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-18 11:57:59 -0800

    progress

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // TODO
+var TodoList = (props) => {
+    var onListItemClick = (event) => {
+        console.log('I got Clicked')
+    }
+    return (
+    <ul>
+        <li onClick={onListItemClick}>{props.todos[0]}</li>
+        <li>{props.todos[1]}</li>
+        <li>{props.todos[2]}</li>
+    </ul>
+    )
+};
+var App = () => (
+    <div>
+        <h1>List</h1>
+        <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+        
+    </div>    
+    );
+
+  ReactDOM.render(<App />, document.getElementById("app"));
